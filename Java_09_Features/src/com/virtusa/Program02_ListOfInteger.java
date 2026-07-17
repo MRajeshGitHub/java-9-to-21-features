28a44ba7350d92b2de53ccdf0628b62e32ce4da5
Rajesh <rajeshyaboo2008@gmail.com>
2026-07-17 10:27:57 +0530

    Program02 add list.of whit numbers

---
+++ Java_09_Features/src/com/virtusa/Program02_ListOfInteger.java
@@ -0,0 +1,13 @@
+package com.virtusa;
+
+import java.util.List;
+
+public class Program02_ListOfInteger {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> nums= List.of(1,2,3,4,5,6,7,8,9);
+		System.out.println("Num form list is : "+nums);
+	}
+
+}
