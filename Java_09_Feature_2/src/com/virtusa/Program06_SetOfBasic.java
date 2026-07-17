1b0fa34be6f3901a5ab9b3c6c83ff40c41c6a8bf
Rajesh <rajeshyaboo2008@gmail.com>
2026-07-17 12:41:21 +0530

    feat : add Java 9 - Program06 Set.of() Basic

---
+++ Java_09_Feature_2/src/com/virtusa/Program06_SetOfBasic.java
@@ -0,0 +1,12 @@
+package com.virtusa;
+
+import java.util.Set;
+
+public class Program06_SetOfBasic {
+
+	public static void main(String[] args) {
+		
+		Set<String> tech=Set.of("Java","Python","C#","Ruby");
+		System.out.println("Techonology are : "+tech);
+	}
+}
